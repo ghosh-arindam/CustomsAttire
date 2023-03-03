3181522290c797d958c814251be91f60d5e446e4
Arindam Ghosh <aaarindam@gmail.com>
2023-03-03 17:33:17 +0530

    Initial Changes

---
+++ Client/customsattire.ui/src/scenes/purchaseorders/index.jsx
@@ -0,0 +1,98 @@
+// import React, { useState, useEffect } from "react";
+// import { Box, Typography, useTheme } from "@mui/material";
+// import { DataGrid } from "@mui/x-data-grid";
+// import { tokens } from "../../theme";
+// import Header from "../../components/Header";
+// import { useDispatch, useSelector } from "react-redux";
+
+// const PurchaseOrders = () => {
+//   const theme = useTheme();
+//   const colors = tokens(theme.palette.mode);
+//   //   const [purchaseorders, setPurchaseorders] = useState();
+
+//   const [pageSize, setPageSize] = useState(5);
+//   const [rowId, setrowId] = useState();
+//   const dispatch = useDispatch();
+//   const PurchaseOrdersState = useSelector((state) => state.PurchaseOrdersState);
+//   useEffect(() => {
+//     dispatch(loadCustomers());
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, []);
+//   const columns = [
+//     { field: "id", headerName: "ID" },
+//     {
+//       field: "name",
+//       headerName: "Name",
+//       flex: 1,
+//       cellClassName: "name-column--cell",
+//     },
+//     {
+//       field: "phone",
+//       headerName: "Phone Number",
+//       flex: 1,
+//     },
+//     {
+//       field: "email",
+//       headerName: "Email",
+//       flex: 1,
+//     },
+//     {
+//       field: "cost",
+//       headerName: "Cost",
+//       flex: 1,
+//       renderCell: (params) => (
+//         <Typography color={colors.greenAccent[500]}>
+//           ${params.row.cost}
+//         </Typography>
+//       ),
+//     },
+//     {
+//       field: "date",
+//       headerName: "Date",
+//       flex: 1,
+//     },
+//   ];
+
+//   return (
+//     <Box m="20px">
+//       <Header title="PURCHASE ORDERS" subtitle="List of Purchase Orders Made" />
+//       <Box
+//         m="40px 0 0 0"
+//         height="75vh"
+//         sx={{
+//           "& .MuiDataGrid-root": {
+//             border: "none",
+//           },
+//           "& .MuiDataGrid-cell": {
+//             borderBottom: "none",
+//           },
+//           "& .name-column--cell": {
+//             color: colors.greenAccent[300],
+//           },
+//           "& .MuiDataGrid-columnHeaders": {
+//             backgroundColor: colors.blueAccent[700],
+//             borderBottom: "none",
+//           },
+//           "& .MuiDataGrid-virtualScroller": {
+//             backgroundColor: colors.primary[400],
+//           },
+//           "& .MuiDataGrid-footerContainer": {
+//             borderTop: "none",
+//             backgroundColor: colors.blueAccent[700],
+//           },
+//           "& .MuiCheckbox-root": {
+//             color: `${colors.greenAccent[200]} !important`,
+//           },
+//         }}
+//       >
+//         <DataGrid
+//           checkboxSelection
+//           rows={fetchPurchaseOrders}
+//           columns={columns}
+//         />
+//       </Box>
+//     </Box>
+//   );
+// };
+
+// export default PurchaseOrders;
