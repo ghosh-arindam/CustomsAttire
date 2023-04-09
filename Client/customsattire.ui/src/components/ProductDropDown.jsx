27d9b681b9a1c361e69f86f8ff977ecce7cfcdbd
Arindam Ghosh <aaarindam@gmail.com>
2023-04-10 00:01:08 +0530

    Product DropDown Component

--- Client/customsattire.ui/src/components/ProductDropDown.jsx
+++ Client/customsattire.ui/src/components/ProductDropDown.jsx
@@ -1,56 +1,64 @@
-import { useState, useEffect, memo } from "react";
-import { Box, TextField, MenuItem } from "@mui/material";
+import { useState, useEffect } from "react";
+import { Autocomplete } from "@mui/material";
+import { TextField } from "@mui/material";
 import { useDispatch, useSelector } from "react-redux";
 import { loadProduct } from "../redux/action";
 
 const ProductDropDownComponent = (props) => {
-  const [fabricState, setFabricState] = useState();
+  const [selectedProduct, setSelectedProduct] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
   const { Products } = useSelector((state) => state.data);
   const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(loadProduct());
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-  const handleSelectFabricChange = (event) => {
-    console.log(event.target.value);
-    // Here, we invoke the callback with the new value
-    props.onChange(event.target.value);
-    setFabricState(event.target.value);
+  useEffect(
+    () => {
+      dispatch(loadProduct());
+    },
+    //eslint-disable-next-line react-hooks/exhaustive-deps
+    []
+  );
+
+  // const handleChange = (value) => {
+  //   console.log("Selected value:", value);
+  // };
+
+  const handleProductChange = (event, value) => {
+    setSelectedProduct(value);
+    if (typeof props.onChange === "function") {
+      props.onChange(value);
+    }
+    console.log(value.fabricCode, value.description);
   };
+
+  const handleSearchInputChange = (event) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredProducts = Products?.filter((product) =>
+    product.fabricCode.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  //const { onChange = handleChange } = props;
+
   return (
-    <Box>
-      <TextField
-        id="outlined-select"
-        select
-        label="Fabric Code"
-        variant="outlined"
-        style={{ width: 300 }}
-        onChange={handleSelectFabricChange}
-        InputLabelProps={{
-          shrink: true,
-        }}
-        value={fabricState || ""}
-        defaultValue={""}
-        name="fabricCode"
-        sx={{ gridColumn: "span 4" }}
-      >
-        {Products?.map((d, index) => (
-          <MenuItem
-            key={index}
-            value={JSON.stringify({
-              name: d.fabricCode,
-              description: d.description,
-            })}
-            //value={props.d.description}
-          >
-            {d.fabricCode} - {d.description}
-          </MenuItem>
-        ))}
-        :<div></div>
-      </TextField>
-    </Box>
+    <Autocomplete
+      value={selectedProduct}
+      onChange={handleProductChange}
+      options={filteredProducts || []}
+      getOptionLabel={(option) =>
+        option.fabricCode + " - " + option.description
+      }
+      renderInput={(params) => (
+        <TextField
+          {...params}
+          label="Fabric Code"
+          variant="outlined"
+          style={{ width: 300 }}
+          onInput={handleSearchInputChange}
+        />
+      )}
+    />
   );
 };
 
-export default memo(ProductDropDownComponent);
+export default ProductDropDownComponent;
